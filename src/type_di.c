18bf0722188de5327c270f5c00010fe19e34be2f
Matthieu DAVID <mdavid@e1r11p11.42.fr>
2020-02-21 15:00:41 +0100

    wip: type_di

--- src/type_di.c
+++ src/type_di.c
@@ -1,7 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   type_di.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdavid <mdavid@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/21 14:59:08 by mdavid            #+#    #+#             */
+/*   Updated: 2020/02/21 14:59:57 by mdavid           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "ft_printf.h"
 
 void	type_int(t_data *t)
 {
-	(void)t;
+	char	*val;
+	int		sign;
+	int		l;
+
+	val = ft_itoa(va_arg(t->valist, int));
+	sign = (val[0] == '-') ? -1 : 0;
+	if (t->flag.width >= (int)ft_strlen(val) + t->flag.plus + sign)
+		if (!(t->bf = ft_strnew(t->flag.width)))
+			return
+	
+	(t->flag.plus == 1 && val[0] != '-') ? += 1 : 0;
+
+
 }
